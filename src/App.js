1c08a4e184454d7a9da95c2313e352c699fef73d
JoelShajan <joelshajan11@gmail.com>
2022-12-28 18:00:20 +0530

    routed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-
+import {Switch,Route} from 'react-router-dom'
 
 function App() {
-  return (
-    <div >
-      Movie starts now!
-    </div>
-  );
+  return <Switch>
+    <Route exact={true} path="/">
+      This is the Beginning
+    </Route>
+    <Route exact path="/starred">
+      This is star
+    </Route>
+    <Route>
+      Undefined
+    </Route>
+  </Switch>
 }
 
 export default App;
