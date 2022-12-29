98aaf2ab94a0c953a77d5f8a09af26e97e1cc179
JoelShajan <joelshajan11@gmail.com>
2022-12-29 15:52:32 +0530

    api added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,17 @@ import { apiGet } from '../misc/config';
 const Home = () => {
     const [input,setinput]=useState('');
     const [result,setresult]=useState(null);
+    const [option,setoption]=useState('shows');
+    const [current,setcurrent]=useState('shows');
     const handelInput=(ev)=>{
         
         setinput(ev.target.value)
         //console.log(document.getElementById('INP').value);
     };
     const handleSearch=(input)=>{
-       apiGet(`/search/shows?q=${input}`).then(result=>{
-       setresult(result); 
+       apiGet(`/search/${option}?q=${input}`).then(result=>{
+       setcurrent(option);
+        setresult(result); 
        console.log(result);
        }); 
        
@@ -30,25 +33,45 @@ const Home = () => {
         }
         else {
             return (result.map(item=>{
-                return (
-                    <div key={item.show.id}>
+                if (current==='shows'){
+                 return (
+                <div key={item.show.id}>
                         {item.show.name}
-                    </div>
-                );
-            }
-            )
-            );
-        }
+                        </div>)  }
+                else{
+                    return (
+                        <div key={item.person.id}>
+                        {item.person.name}
+                    </div>);
+                }
+            })
+            )}
+
+           
+        
+        
+    };
+    const selector=(ev)=>{
+            setoption(ev.target.value)   ; 
     };
   return (
     <MainPageLayout>
-        <input id='INP' type='text' onChange={(ev)=>{
+        <input id='INP' type='text' 
+        placeholder='Enter search key'
+        onChange={(ev)=>{
             handelInput(ev);
         }} value={input} onKeyDown={keyhandle}/>
         <button onClick={()=>{handleSearch(input);}}>
             Search
         </button>
-
+        <label>
+            Shows
+            <input type='radio' value='shows' name="selector" onChange={selector}/>
+        </label>
+        <label>
+            Actors
+            <input type='radio' value='people' name="selector" onChange={selector} />
+        </label>
         <div>{resultRender()}</div>
     </MainPageLayout>
     
