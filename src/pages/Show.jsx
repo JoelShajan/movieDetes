bb24aea848c6c635c686f0940f8372c4d852e8e6
JoelShajan <joelshajan11@gmail.com>
2022-12-30 15:54:17 +0530

    added spin thingy base

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -4,14 +4,35 @@ import { apiGet } from '../misc/config';
 const Show = () => {
     const {id}=useParams();
     const [show,setShow]=useState(null);
+    const [isloading,setloading]=useState(true);
+    const [error,seterror]=useState(null);
     useEffect(()=>{
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results=>{
+           setTimeout(()=>{
             setShow(results);
-        })
+            setloading(false);
+           },1500)
+            
+        }).catch(err=>{
+            seterror(err.message);
+            setloading(false);
+        });
     },[id]);
    console.log(show);
+   if(error){
+    return (
+      <div>
+        {error}
+      </div>
+    )
+   }
+   if (isloading){
+    return (
+        <div>Show data is loading</div>
+      )
+   }
   return (
-    <div>Show</div>
+    <div>Show data has loaded</div>
   )
 }
 export default Show
