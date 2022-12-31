fa2db19f8712dde898f722c788005f85c751b3f5
JoelShajan <joelshajan11@gmail.com>
2022-12-31 10:38:56 +0530

    new reducer added

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,32 +1,38 @@
-import React,{useEffect,useState} from 'react';
+import React,{useEffect,useReducer} from 'react';
 import { useParams } from 'react-router';
 import { apiGet } from '../misc/config';
+
+const reducer=(prevState,action)=>{
+  switch(action.type){
+    case 'FETCH_SUCESS':{
+    return ({isLoading:false,show:action.show});
+    }
+    default: return prevState;
+  }
+}
+const initialState={
+  show:null,
+  isloading:true,
+}
+
 const Show = () => {
     const {id}=useParams();
-    const [show,setShow]=useState(null);
-    const [isloading,setloading]=useState(true);
-    const [error,seterror]=useState(null);
+    const [state,dispatch] = useReducer(reducer,initialState);
+    // const [show,setShow]=useState(null);
+    // const [isloading,setloading]=useState(true);
+    // const [error,seterror]=useState(null);
+    console.log(state);
     useEffect(()=>{
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results=>{
-           setTimeout(()=>{
-            setShow(results);
-            setloading(false);
-           },1500)
+           
+            dispatch({type:'FETCH_SUCESS',show:results});
+          
             
-        }).catch(err=>{
-            seterror(err.message);
-            setloading(false);
-        });
+        })
     },[id]);
-   console.log(show);
-   if(error){
-    return (
-      <div>
-        {error}
-      </div>
-    )
-   }
-   if (isloading){
+   //console.log(show);
+   
+  if (state.isloading){
     return (
         <div>Show data is loading</div>
       )
